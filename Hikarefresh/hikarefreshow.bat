c53324e028893144b3fc397880661a67d789a352
Bionic Butter <44027930+Bionic-OSE@users.noreply.github.com>
2023-03-27 20:40:38 +0700

    22107.200_stable
    Isn't really stable yet. The main purpose is to test the servicing pipeline.

--- Hikarefresh/hikarefreshow.bat
+++ Hikarefresh/hikarefreshow.bat
@@ -1,7 +1,7 @@
 ::[Bat To Exe Converter]
 ::
 ::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJHuR4FY1OidzRRC+HmK1CLw4w9225+OMo18IaMo2c47J07eBLtw7407lZ58u2XQUkcgDbA==
+::fBw5plQjdCyDJHuR4FY1OidzRRC+HmK1CLw4w9225+OMo18IaMo2c47Jz4imKesS+Eznepg4xjRTm8Rs
 ::YAwzuBVtJxjWCl3EqQJgSA==
 ::ZR4luwNxJguZRRnk
 ::Yhs/ulQjdF+5
@@ -17,25 +17,24 @@
 ::cxY6rQJ7JhzQF1fEqQJieEsaHWQ=
 ::ZQ05rAF9IBncCkqN+0xwdVsGHFTMbQs=
 ::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::eg0/rx1wNQPfEVWB+kM9LVsJDCyLN2qoA7MO7fvz4P3Jp1UYNA==
 ::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQISIRRaRSCpCSuLE7wf46jW6uSSwg==
+::cRolqwZ3JBvQF1fEqQISIRRaRSCpCSuLE7wf46jW6uSS4ngNUOMrfZ27
 ::dhA7uBVwLU+EWHuN+0w5K1t2WRCWOXna
 ::YQ03rBFzNR3SWATElA==
 ::dhAmsQZ3MwfNWATElA==
 ::ZQ0/vhVqMQ3MEVWAtB9wSA==
 ::Zg8zqx1/OA3MEVWAtB9wSA==
 ::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJHuR4FY1OidzRRCOPWmGJLwT5uHfxN/KpF8cWecod53Q5paHL+4azm7ROJ4k3XtIjcYHMDNRdRO5ezMcoGVDpHG0GomZqwqB
+::Zh4grVQjdCyDJHuR4FY1OidzRRCOPWmGJLwT5uHfxN/KpF8cWecod53Q5paHL+4azm7ROJ4k3XtIjcYHMDNRdRO5awkmrH1KikyMPMaOtgnzT1uB6Vx+Hn1x5w==
 ::YB416Ek+ZW8=
 ::
 ::
 ::978f952a14a936cc963da21a135fa983
+
 @echo off
 
-title Please wait...
-echo BioniDKU Quick Menu
-echo(
-echo Preparing Quick Menu options...
-powershell.exe %systemdrive%\Bionic\Hikaru\HikaruQM.ps1
-exit
+dir %systemdrive%\Bionic\Hikarefresh\hikarefreshow.ps1 > nul
+if %errorlevel%==1 exit
+
+powershell -Command "& $env:SYSTEMDRIVE\Bionic\Hikarefresh\hikarefreshow.ps1"
